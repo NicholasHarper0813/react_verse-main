bc4e12527fc1bd1becba7f4478fa944c80f5ee8b
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 06:14:16 -0800

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,23 +1,6 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package ReactVerse
- * Template Name: Frontpage
- */
-
-get_header(); ?>
-
+<?php get_header(); ?>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main" aria-live="assertive" tabindex="-1"></main><!-- #main -->
-	</div><!-- #primary -->
-
+		<main id="main" class="site-main" role="main" aria-live="assertive" tabindex="-1"></main>
+	</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

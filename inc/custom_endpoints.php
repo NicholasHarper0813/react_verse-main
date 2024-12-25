6b59a87e346873fa1eae19ae806f8166644bb9f8
nicholas harper <nicholasharper000@gmail.com>
2024-12-24 18:13:16 -0800

    Update custom_endpoints.php

--- inc/custom_endpoints.php
+++ inc/custom_endpoints.php
@@ -1,10 +1,10 @@
 <?php
-function reactverse_get_theme_mod( WP_REST_Request $request ) {
+function reactverse_get_theme_mod( WP_REST_Request $request ) 
+{
 	$param = $request['value'];
 	$imageUrl = get_theme_mod($param);
 	$mediaData =  attachment_url_to_postid($imageUrl);
 	$altText = get_post_meta( $mediaData, '_wp_attachment_image_alt', true );
-
 	$data = array (
 		'url' => $imageUrl,
 		'alt' => $altText
